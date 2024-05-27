2fb0adf97aec38faf9702613244ca4c693d19d31
Alex <alexvdvelden@hotmail.com>
2024-05-27 21:54:50 +0200

    Add project files.

---
+++ ShellFlyoutWindowsBug/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ShellFlyoutWindowsBug
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            SetTheme();
+        }
+
+        private void SetTheme()
+        {
+            UserAppTheme = AppTheme.Light;
+        }
+    }
+}
